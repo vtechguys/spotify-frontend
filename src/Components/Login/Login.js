9e5133f16c29e763bf98dc555e55b301d2b606fe
Ashu Ghildiyal <ashughildiyal@Ashus-Air.lan>
2018-11-01 20:03:39 +0530

    added validation to login and SignUp form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,17 +4,30 @@ import {Route,Link} from 'react-router-dom'
 import './Login.css'
 import Input from '../UI/Input/Input'
 import Button from '../UI/Button/Button'
-export const Login =()=>{
-
+export const Login =(props)=>{
+    let formElements=[];
+    for(let key in props.loginForm)
+    {
+        formElements.push({id:key,config:props.loginForm[key]})
+    }
    
         return(
             <div className='col-md-6 Login'>
             <h1>Login</h1>
-            <Input type='email' id='loginEmail'>Email</Input>
-            <Input type='password' id='loginPassword'>Password</Input>
-            <div className='float-left' style={{margin:5}}></div>
-            <div className='float-right' style={{margin:5}}></div>
-            <Button type='submit' className='primary'>Login</Button>
+            <form className="form"  onSubmit={(event)=>props.submit(event,"loginForm")}>
+            {formElements.map((formElement)=>{
+                return <Input label={formElement.id} key={formElement.id} elementType={formElement.config.elementType} 
+                value={formElement.value}
+                elementConfig={formElement.config.elementConfig}
+                invalid={!formElement.config.valid}
+                touched={formElement.config.touched}
+                message={formElement.config.message}
+                changed={(event)=>props.changed(event,formElement.id,'loginForm')}/>
+            })
+         }
+         <Button type='submit' className='primary'>Login</Button>
+         </form>
+        
             </div>
         )
     
--- src/config/registeredUrls.js
+++ src/config/registeredUrls.js
@@ -3,8 +3,8 @@ export const apiUrls = {
     //First call to webindex
     WEB_INDEX:'webindex',
     //Auth urls
-    SIGN_IN:'users/login',
-    SIGN_UP:'users/register',
+    SIGN_IN:'/users/login',
+    SIGN_UP:'/users/register',
     FORGOT_PASSWORD:'users/forgot-password',
     RESET_PASSWORD:'users/reset-password',
     SIGN_OUT:'logout',
